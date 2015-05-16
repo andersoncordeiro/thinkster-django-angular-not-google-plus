55b51492cdcb3564b34714229d1aa74c4341829a
Oussama Krifa <krifa.oussama@gmail.com>
2015-05-16 17:45:45 +0100

    Fully implemented the authentication feature (log in/out)

---
+++ static/javascripts/authentication/authentication.module.js
@@ -0,0 +1,18 @@
+/**
+ * Created by oussama on 5/15/15.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.authentication', [
+      'thinkster.authentication.controllers',
+      'thinkster.authentication.services'
+    ]);
+
+  angular
+    .module('thinkster.authentication.controllers', []);
+
+  angular
+    .module('thinkster.authentication.services', ['ngCookies']);
+})();
---
+++ static/javascripts/layout/layout.module.js
@@ -0,0 +1,14 @@
+/**
+ * Created by oussama on 5/16/15.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.layout', [
+      'thinkster.layout.controllers'
+    ]);
+
+  angular
+    .module('thinkster.layout.controllers', []);
+})();
